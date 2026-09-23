02255322089c991d38dde1df5ff2a7dab3ba9248
Ali Reza <alirazahalani@gmail.com>
2026-09-23 17:44:11 +0400

    register view update

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,4 +1,5 @@
 import functools
+from sqlite3 import IntegrityError
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
@@ -11,5 +12,26 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
-    return 'register page'
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
 
+        if not username:
+            error = "Username is required"
+        elif not password:
+            error = "Password is required"
+
+        if error is None:
+            try:
+                db.execute('Insert into user(username, password) values (?,?)',
+                           (username, generate_password_hash(password)))
+                db.commit()
+            except db.IntegrityError:
+                error = f"User (username) is already registerd."
+            else:
+                return redirect(url_for('auth.login'))
+        flash(error)
+    return render_template('auth/register.html')
+    
