f477e601140f86104cf83fe6ca0df7732d2f68fb
Ali Reza <alirazahalani@gmail.com>
2026-09-23 18:54:59 +0400

    001

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -59,7 +59,17 @@ def login():
         flash(error)
     return render_template('auth/login.html')
     
+@bp.before_app_request
+def load_logged_user():
+    user_id = session.get('user_id')
 
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'select * from user where user_id  = ?', (user_id,)
+        ).fetchone()
+        
 
     
     
