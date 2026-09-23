3fc5e27a3876ff3f9f33a3d770dad2334c9cce60
Ali Reza <alirazahalani@gmail.com>
2026-09-23 18:35:55 +0400

    login view

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -34,4 +34,33 @@ def register():
                 return redirect(url_for('auth.login'))
         flash(error)
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        user = db.execute('select * from user where username = ?', (username,)).fetchone()
+        error = None
+
+        if username is None:
+            error = "Incorrect username"
+        elif not check_password_hash(user['password'], password):
+            error = "Incorrect password"
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect('index')
+
+        flash(error)
+    return render_template('auth/login.html')
+    
+
+
+    
+    
     
